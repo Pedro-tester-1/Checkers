99686097ffe444af47c011ad2cbef58bf69dc7e1
Pedro-tester-1 <assasin.pro639106@gmail.com>
2026-06-11 14:51:35 -0500

    names

--- main_functions.py
+++ main_functions.py
@@ -1,13 +1,26 @@
 def dibujar_tablero():
     print("  A B C D E F G H")
-    for fila in range(8, 0, -1):
-        print(f"{fila} ", end="")
-        for col in range(8):
-            if (fila + col) % 2 == 0:
+    for f in range(8, 0, -1):
+        print(f"{f} ", end="")
+        for c in range(8):
+            if (f + c) % 2 == 0:
                 print("_ ", end="")
             else:
-                print("  ", end="")
-        print(f" {fila}")
+                print("_ ", end="")
+        print(f" {f}")
     print("  A B C D E F G H")
 
 dibujar_tablero()
+
+def pedir_nombre():
+    nombre = input("Ingrese su nombre: ")
+    for letra in nombre:
+        if letra in "1234567890":
+            print("El nombre no puede tener números. Vamos hacerlo denuevo.")
+            return pedir_nombre()
+        elif letra in "!@#$%^&*+()_+-¿?¡!-=~`[]{}|\\;:'\",.<>/?°¡!":
+            print("El nombre no puede tener caracteres especiales. Vamos hacerlo denuevo.")
+            return pedir_nombre() 
+    return nombre
+
+pedir_nombre()
